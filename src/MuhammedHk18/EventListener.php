1766cdbb58f014bd942316ebc47696c00ca698ee
Muhammed0232 <34399188+Neverpro@users.noreply.github.com>
2023-04-15 20:07:41 +0300

    change namespaces for poggit

--- src/MuhammedHk18/EventListener.php
+++ src/MuhammedHk18/EventListener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ghost;
+namespace MuhammedHk18;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
--- src/MuhammedHk18/Main.php
+++ src/MuhammedHk18/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ghost;
+namespace MuhammedHk18;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
